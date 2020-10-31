34d03a693756d9fc6aac1f329e80d496dca2bd73
izformatika <izformatika@gmail.com>
2020-10-31 18:26:40 +0300

    deleted cpp, deleted obsolete py, everything in signatures.py now, almost done with Q_uniq_spec_position

--- words/signatures.py
+++ words/signatures.py
@@ -7,27 +7,149 @@ def vowcount(str):
         if i in vowels:
             c+=1
     return c
-f=open("russian_nouns.txt","r",encoding="UTF8")
-
 l=[]
-l=f.readlines()
-f.close()
-for i in l:
-    signature=list(Counter(i[:-1]).values())
-    signature.sort()
-    f1=open(str(signature)+".txt","a+")
-    f1.write(i)
-    f1.close()
 
+def all_to_uniq_nuniq():
+    f=open("russian_nouns.txt","r",encoding="UTF8")
+    l=f.readlines()
+    f.close()
+    uniq = open("uniq.txt","a+",encoding="UTF8")
+    nuniq = open("nuniq.txt","a+",encoding="UTF8")
+    for i in l:
+        signature=list(Counter(i[:-1]).values())
+        if len(signature) == len(i) - 1:
+            uniq.write(i)
+        else:
+            nuniq.write(i)
+    uniq.close()
+    nuniq.close()
 
-'''l=[]
-l=f.readlines()
-f.close()
-for i in l:
-    if len(set(i))==len(i) and "ь" not in i and "ъ" not in i:
-        vow=vowcount(i)
-        cons=len(i)-vow
-        f1=open("vow "+str(vow)+" cons "+str(cons)+".txt", "a+")
-        f1.write(i)
-        f1.close()
-'''
+def uniq_to_sig():
+    f=open("uniq.txt","r",encoding="UTF8")
+    l=[]
+    l=f.readlines()
+    f.close()
+    j = 0
+    for i in l:
+        vow_count = 0
+        spec_count = 0
+        for let in i[:-1]:
+            if let in vowels:
+                vow_count += 1
+            elif let in special:
+                spec_count += 1
+        file=open("sig/uniq/vow "+str(vow_count)+" cons " + str(len(i) - spec_count - vow_count - 1)+" spec " + str(spec_count) + ".txt", "a+",encoding="UTF8")
+        file.write(i)
+        file.close()
+        j += 1
+        if j % 100 == 0:
+            print(str(j) + ' of ' + str(len(l)))
+    print('uniq done')
+
+def nuniq_to_sig():
+    f=open("nuniq.txt","r",encoding="UTF8")
+    l=[]
+    l=f.readlines()
+    f.close()
+    j = 0
+    for i in l:
+        vow_sub = []
+        cons_sub = []
+        spec_sub = []
+        for let in i[:-1]:
+            if let in vowels:
+                vow_sub.append(let)
+            elif let in special:
+                spec_sub.append(let)
+            else:
+                cons_sub.append(let)
+        vow_sig=list(Counter(vow_sub).values())
+        cons_sig=list(Counter(cons_sub).values())
+        spec_sig=list(Counter(spec_sub).values())
+        vow_sig.sort()
+        cons_sig.sort()
+        spec_sig.sort()
+        file=open("sig/nuniq/vow "+str(vow_sig)+" cons "+str(cons_sig)+" spec "+str(spec_sig)+".txt","a+",encoding="UTF8")
+        file.write(i)
+        file.close()
+        j += 1
+        if j % 100 == 0:
+            print(str(j) + ' of ' + str(len(l)))
+from os import walk
+from math import factorial
+def Q_uniq_spec_position(mode):
+    '''
+    mode 0 after vow
+    mode 1 before vow
+    mode 2 not after vow
+    mode 3 not before vow
+    '''
+    flist = []
+    for(dirpath, dirnames, filenames) in walk('sig/debug'):#debug -> uniq
+        flist.extend(filenames)
+    for filename in flist:
+        spec = int(filename[-6:-4].replace('.',''))#unsafe
+        if spec != 1:
+            continue
+        vows = int(filename[4:6])#unsafe
+        cons = int(filename[10:13].replace('.',''))#unsafe
+        length = vows + cons + spec
+        f = open("sig/debug/" + filename, "r", encoding = "UTF8")#debug -> uniq
+        l = f.readlines()
+        f.close()
+        if len(l) == 0:#shouldnt be, otherwise file wouldnt be created
+            continue
+        ans = -1
+        if mode == 0 or mode == 1:
+            ans = vows * factorial(length - 2) * (length - 1)
+        elif mode == 2 or mode == 3:
+            ans = factorial(length) - vows * factorial(length - 2) * (length - 1)
+        else:
+            ans = -1
+        print(len(l))
+        file = open("questions.txt", "w")
+        for i in l:
+            question = str(vows) + " " + str(cons) + " " + str(spec) + " Сколько слов длиной в "+str(length)+" букв (не обязательно осмысленных) можно составить " + \
+                "из букв слова \""+i[:-1]+"\", если в них " + ("мягкий" if "ь" in i else "твёрдый") + " знак должен стоять строго "
+            if mode == 0:
+                question += "после гласной"
+            elif mode == 1:
+                question += "перед гласной"
+            elif mode == 2:
+                question += "не после гласной"
+            elif mode == 3:
+                question += "не перед гласной"
+            else:
+                question += "ОШИБКА"
+            question += ", и при этом буквы из исходного слова можно использовать ровно по одному разу?"
+            shortanswer(i[:-1], question, ans, file)
+        file.close()
+def shortanswer(name, question, answer, file):
+    file.write("<question type=\"shortanswer\">\n")
+    file.write("<name>\n")
+    file.write("<text>" +name+ "</text>\n")
+    file.write("</name>\n")
+    file.write("<questiontext format=\"html\">\n")
+    file.write("<text><![CDATA[<p>" + question + "</p>]]></text>\n")
+    file.write("</questiontext>\n")
+    file.write("<generalfeedback format=\"html\">\n")
+    file.write("<text></text>\n")
+    file.write("</generalfeedback>\n")
+    file.write("<defaultgrade>1.0000000</defaultgrade>\n")
+    file.write("<penalty>0.3333333</penalty>\n")
+    file.write("<hidden>0</hidden>\n")
+    file.write("<idnumber></idnumber>\n")
+    file.write("<usecase>0</usecase>\n")
+    file.write("<answer fraction=\"100\" format=\"moodle_auto_format\">\n")
+    file.write("<text>" + str(answer) + "</text>\n")
+    file.write("<feedback format=\"html\">\n")
+    file.write("<text></text>\n")
+    file.write("</feedback>\n" )
+    file.write("</answer>\n")
+    file.write("</question>\n\n" )
+        
+#uniq_spec_position()
+#print(factorial(5))
+uniq_to_sig()
+            
+#Q_uniq_spec_position(0)
--- words/test.py
+++ words/test.py
@@ -1,12 +1,33 @@
-s="вдагм"
+from collections import Counter
+s="бульдо"
 c=0
 for i1 in s:
-    for i2 in s:
-        for i3 in s:
-            for i4 in s:
-                for i5 in s:
-                    #for i6 in s:
-                    s1=i1+i2+i3+i4+i5#+i6
-                    if not (i1==i2 or i2==i3 or i3==i4 or i4==i5) and ("а" in s1 or "ю" in s1):
-                        c+=1
+    print(i1)
+    for i2 in s.replace(i1, ''):
+        for i3 in s.replace(i1, '').replace(i2, ''):
+            for i4 in s.replace(i1, '').replace(i2, '').replace(i3, ''):
+                for i5 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, ''):
+                    for i6 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, ''):
+                        '''for i7 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, ''):
+                            for i8 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, ''):
+                                for i9 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, ''):
+                                    for i10 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, '').replace(i9, ''):
+                                        for i11 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, '').replace(i9, '').replace(i10, ''):
+                                            for i12 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, '').replace(i9, '').replace(i10, '').replace(i11, ''):
+                                            '''
+                        s1=i1+i2+i3+i4+i5+i6#+i7+i8+i9+i10+i11+i12
+                                                #signature=list(Counter(s1).values())
+                                                #if len(signature) == len(s1) - 1 and
+                        if ("ьу" in s1 or "ьо" in s1):
+                            c+=1
 print (c)
+vows = 2
+cons = 3
+length = 6
+from math import factorial
+print(vows * factorial(length - 2) * (length-1))
+#1*vows*(length-1)*cons!*(vows-1)!
+#ьГ
+i = "фыв"
+question = "Сколько слов длиной в "+length+" букв (не обязательно осмысленных) можно составить " + \
+                "из букв слова \""+i[:-1]+"\", если в них " + "мягкий" if "ь" in i else "твёрдый" + " знак должен стоять строго "
