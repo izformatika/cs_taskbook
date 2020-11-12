80a5ac3e54c96a81e874f2c364b93848f64a7c3e
izformatika <izformatika@gmail.com>
2020-11-12 20:12:47 +0300

    not two last letters, and two first letters not in their positions

--- words/signatures.py
+++ words/signatures.py
@@ -97,7 +97,7 @@ def Q_uniq_spec_position(mode, difficulty):
     diff 0 50 < answer < 500
     diff 1 5000 < answer < 30000
     '''
-    ansdict = {}
+    ansdict = {}#in every question
     flist = []
     for(dirpath, dirnames, filenames) in walk('sig/uniq'):
         flist.extend(filenames)
@@ -115,12 +115,13 @@ def Q_uniq_spec_position(mode, difficulty):
             ans = factorial(length) - vows * factorial(length - 2) * (length - 1)
         else:
             ans = -1
-        if difficululty == 1 and (ans < 5000 or ans > 30000) \
+        if difficulty == 1 and (ans < 5000 or ans > 30000) \
            or difficulty == 0 and (ans < 50 or ans > 500):
             continue
-        if ans not in ansdict.keys():
+        if ans not in ansdict.keys():#in every question
             ansdict[ans] = 0
-        elif ansdict[ans] > 100:
+        elif ansdict[ans] > 30:
+            continue
         f = open("sig/uniq/" + filename, "r", encoding = "UTF8")
         l = f.readlines()
         f.close()
@@ -128,6 +129,12 @@ def Q_uniq_spec_position(mode, difficulty):
             continue
         file = open("spec pos " + str(mode) + " " + str(difficulty) + ".txt", "a+")
         for i in l:
+            if ans not in ansdict.keys():#in every question
+                ansdict[ans] = 0
+            elif ansdict[ans] < 30:
+                ansdict[ans] = ansdict[ans] + 1
+            else:
+                break
             question = "Сколько слов длиной в "+str(length)+ " " + letters(length) + " (не обязательно осмысленных) можно составить " + \
                 "из букв слова \""+i[:-1]+"\", если в них " + ("мягкий" if "ь" in i else "твёрдый") + " знак должен стоять строго "
             if mode == 0:
@@ -143,11 +150,17 @@ def Q_uniq_spec_position(mode, difficulty):
             question += ", и при этом буквы из исходного слова можно использовать ровно по одному разу?"
             shortanswer(i[:-1], question, ans, file)
         file.close()
-def Q_not_two_conseq(difficulty):
+def Q_uniq_two_conseq(mode, difficulty):# Q_uniq_several_seq , seq, not_first):
     '''
+    mode 0 l1l2 not allowed
+    mode 1 l1l2 necessary
     diff 0 50 < answer < 500
     diff 1 5000 < answer < 30000
+    seq length of postfix that has to be/has to be missing
+    not_first length of prefix - those letters cant be in first not_first letters
     '''
+    not_first = 2
+    ansdict = {}#in every question
     flist = []
     for(dirpath, dirnames, filenames) in walk('sig/uniq'):
         flist.extend(filenames)
@@ -156,19 +169,39 @@ def Q_not_two_conseq(difficulty):
         vows = int(filename[4:6])
         cons = int(filename[10:13].replace('.',''))
         length = vows + cons + spec
-        ans = (length - 2) * factorial(length - 2)
-        '''if difficulty == 1 and (ans < 5000 or ans > 30000) \
+        if length <= 2:
+            continue
+        ans = 0
+        if mode == 0:
+            ans = (length - 1)*factorial(length - 1)
+        else:
+            ans = factorial(length - 1)
+        if difficulty == 1 and (ans < 5000 or ans > 30000) \
            or difficulty == 0 and (ans < 50 or ans > 500):
-            continue'''
+            continue
+        if ans not in ansdict.keys():#in every question
+            ansdict[ans] = 0
+        elif ansdict[ans] > 30:
+            continue
         f = open("sig/uniq/" + filename, "r", encoding = "UTF8")
         l = f.readlines()
         f.close()
         if len(l) == 0:#shouldnt be, otherwise file wouldnt be created
             continue
-        file = open("not two conseq " + str(difficulty) + ".txt", "a+")
+        file = open("uniq two conseq " +str(mode) + ' ' + str(difficulty) + ' ' + str(not_first) + ".txt", "a+")
         for i in l:
+            if ans not in ansdict.keys():#in every question
+                ansdict[ans] = 0
+            elif ansdict[ans] < 30:
+                ansdict[ans] = ansdict[ans] + 1
+            else:
+                break
             question = "Сколько слов длиной в "+str(length)+ " " + letters(length) + " (не обязательно осмысленных) можно составить " + \
-                "из букв слова \""+i[:-1]+"\", если в них не должно встречаться сочетание \"" + i[:2] + "\", и при этом буквы из исходного слова можно использовать ровно по одному разу?"
+                "из букв слова \""+i[:-1]+"\", если в них " + ("не" if mode == 0 else "обязательно") + " должно встречаться сочетание \"" + i[-3:-1] + "\", и при этом буквы из исходного слова можно использовать ровно по одному разу?"
+            if not_first == 1:
+                question +=" При этом слова не должны начинаться с буквы " + i[0]+ "."
+            elif not_first > 1:
+                question +=" При этом буквы " + str(['\'' + str(j) + '\'' for j in i[:not_first]])[2:-2].replace("\"","") + " не должны встречаться на первых " + str(not_first) + " позициях."
             shortanswer(i[:-1], question, ans, file)
         file.close()
 
@@ -200,4 +233,14 @@ def shortanswer(name, question, answer, file):
 #print(factorial(5))
 #uniq_to_sig()
 
-Q_not_two_conseq(0)
+#Q_not_two_conseq(1)
+'''
+for i in range(4):
+    Q_uniq_spec_position(i, 0)
+    Q_uniq_spec_position(i, 1)
+'''
+for i in range(1):
+    Q_uniq_two_conseq(0, 0)#, i)
+    Q_uniq_two_conseq(1, 0)#, i)
+    Q_uniq_two_conseq(0, 1)#, i)
+    Q_uniq_two_conseq(1, 1)#, i)
