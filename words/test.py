9f68533f8e693a58ba28ef229af5f7a5f7c48944
izformatika <izformatika@gmail.com>
2020-12-26 17:41:41 +0300

    fixing...

--- words/test.py
+++ words/test.py
@@ -1,8 +1,19 @@
 # coding=cp1251
 def f1():
+    def good(s2):
+        isgood = False
+        ''' if s[2] not in vowels and s[3] in vowels and s[4] in vowels and s[5] not in vowels:
+            isgood=True
+        else:
+            return False'''
+        for i in range(len(s2)-3):
+            if (i == 0 or s2[i-1] not in vowels) and s2[i] in vowels and s2[i+1] in vowels and s2[i+2] in vowels and s2[i+3] in vowels and (i == len(s2)-4 or s2[i+4] not in vowels):
+                isgood = True
+                break
+        return isgood
     from collections import Counter
     vowels=["а","я","у","ю","э","е","о","ё","ы","и"]
-    s="айцкшж"
+    s="ауыежцфхкъ"
     plus=0
     minus = 0
     #print(s[:2])
@@ -13,19 +24,18 @@ def f1():
                 for i4 in s.replace(i1, '').replace(i2, '').replace(i3, ''):
                     for i5 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, ''):
                         for i6 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, ''):
-                            #for i7 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, ''):
-                                #for i8 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, ''):
-                                    #for i9 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, ''):
-                                        #for i10 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, '').replace(i9, ''):
-                            s1=i1+i2+i3+i4+i5+i6#+i7#+i8+i9+i10#+i11+i12+i13+i14
-                            found = False
-                            for i in range(len(s1)-1):
-                                if s1[i] in vowels and s1[i+1] in vowels:
-                                    plus+=1
-                                    found = True
-                                    break
-                            if not found:
-                                minus+=1
+                            for i7 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, ''):
+                                for i8 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, ''):
+                                    for i9 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, ''):
+                                        for i10 in s.replace(i1, '').replace(i2, '').replace(i3, '').replace(i4, '').replace(i5, '').replace(i6, '').replace(i7, '').replace(i8, '').replace(i9, ''):
+                                            s1=i1+i2+i3+i4+i5+i6+i7+i8+i9+i10#+i11+i12+i13+i14                   
+                                            if good(s1):
+                                                #print(s1)
+                                                plus+=1
+                                            else:
+                                                #print(s1)
+                                                minus+=1
+                                    
     print (plus, minus)
 '''
 4 6 2 True	2574720
@@ -177,21 +187,47 @@ print(f4(9, -2, -3, 0),'=209040')
 #print(f4(8, -3, 2, 0),'=1008')
 #print(f4(8, -3, 4, 0),'=30')
 
-#f1()
 
 #восьмёркаы 3024000 604800
 
 from math import factorial
 
 def seq(tgt, other, qtty, must,lvl=0):
-    if tgt == 2 and other == 5 and qtty == 2:
-        a=1
+    if other<0:
+        return 0
     ans = 0
     length = tgt+other
     if must:
-        for i in range(qtty, tgt+1):
-            ans+=2*factorial(tgt)//factorial(tgt-i)*(other)*seq(tgt-i, other-1, qtty, False,lvl+1)
-            ans+=factorial(tgt)//factorial(tgt-i)*(other)*(other-1)*(length-i-1)*seq(tgt-i, other-2, qtty, False,lvl+1)
+        if tgt >= qtty and other == 0:
+            ans = factorial(tgt)
+        elif tgt == qtty:
+            ans = factorial(other+1)*factorial(tgt)
+        else:
+            tmp=factorial(tgt)//factorial(tgt-qtty)*(other)*factorial(length-qtty-1)
+            print(tmp)
+            ans+=tmp
+            tmp=factorial(tgt)//factorial(tgt-qtty)*(other)*seq(tgt-qtty,other-1,qtty,False)
+            print(tmp)
+            ans+=tmp
+            for j in range(length-qtty-2+1):#before otto
+                after = length-qtty-j-2
+                print(f'[{j}][{after}]')
+                for k in range(max(0,tgt-qtty-after), min(tgt-qtty+1, j+1)):#how many tgt goes to before otto
+                    tmp=factorial(tgt)//factorial(tgt-qtty)*(other)*(other-1)*seq(k, j-k, qtty, False, lvl+1)*factorial(length-qtty-j-2)
+                    print('\t',tmp)
+                    #if k>1:
+                    tmp*=(factorial(tgt-qtty)//factorial(k)//factorial(tgt-qtty-k))
+                    print('\t',tmp)
+                    #if j-k>1:
+                    tmp*=(factorial(other-2)//factorial(j-k)//factorial(other-2-j+k))
+                    print('\t\t',tmp)
+                    ans+=tmp
+            print('!',ans)
+            for i in range(qtty+1, tgt+1):
+                tmp=seq(tgt,other,i,must,lvl+1)
+                print(tmp)
+                ans+=tmp
+                
         print(lvl*'\t',tgt, other, qtty, must, '\t',ans)
         return(ans)
     else:#not must
@@ -199,8 +235,27 @@ def seq(tgt, other, qtty, must,lvl=0):
             ans = factorial(tgt+other)
         else:
             ans = factorial(tgt+other) - seq(tgt,other,qtty,True,lvl+1)
-        print(lvl*'\t',tgt, other, qtty, must, '\t',ans)
+        #print(lvl*'\t',tgt, other, qtty, must, '\t',ans)
         return(ans)
 
+f1()
 
-print(seq(4, 6, 2, True))
+'''
+#print(seq(2, 2, 2, True))
+#print(seq(2, 2, 2, False))
+assert(seq(2, 4, 2, True) == 240)
+assert(seq(3, 2, 2, True)==108)
+assert(seq(3, 3, 2, True)==576)
+assert(seq(3, 4, 3, True)==720)
+assert(seq(3, 4, 2, True)==3600)
+assert(seq(3, 5, 2, True)==25920)
+assert(seq(3, 6, 2, True)==211680)
+print()
+print()
+print()
+'''
+#assert(seq(4, 6, 2, True)==3024000)#362880  259200 259200 241920 ... 259200
+#
+#2177280 only 2
+#725760 only 3
+#120960 only 4
