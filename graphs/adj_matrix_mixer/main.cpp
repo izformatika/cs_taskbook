1ecc51e009593521b7bf5faa658162a78a72d809
ioann <no@no.no>
2021-03-02 19:39:44 +0300

    shortest way goddamn WORKS

--- graphs/adj_matrix_mixer/main.cpp
+++ graphs/adj_matrix_mixer/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include <clocale>
 #include <iomanip>
 #include <fstream>
@@ -33,7 +34,7 @@ void fill_matrix_1(vector<vector<int>> &matrix, vector<string> &labels, int &pic
     matrix[i][j]=1;
     for (size_t i=0;i<matrix.size(); i++)
     for (size_t j=0;j<matrix.size(); j++)
-        if (matrix[i][j]==1) matrix[j][i]=1;
+        if (matrix[i][j]>0) matrix[j][i]=matrix[i][j];
 }
 
 void fill_matrix_2(vector<vector<int>> &matrix, vector<string> &labels, int &pic)
@@ -60,7 +61,7 @@ void fill_matrix_2(vector<vector<int>> &matrix, vector<string> &labels, int &pic
     matrix[i][j]=1;
     for (size_t i=0;i<matrix.size(); i++)
     for (size_t j=0;j<matrix.size(); j++)
-        if (matrix[i][j]==1) matrix[j][i]=1;
+        if (matrix[i][j]>0) matrix[j][i]=matrix[i][j];
 }
 
 void fill_matrix_3(vector<vector<int>> &matrix, vector<string> &labels, int &pic)
@@ -88,7 +89,7 @@ void fill_matrix_3(vector<vector<int>> &matrix, vector<string> &labels, int &pic
     matrix[i][j]=1;
     for (size_t i=0;i<matrix.size(); i++)
     for (size_t j=0;j<matrix.size(); j++)
-        if (matrix[i][j]==1) matrix[j][i]=1;
+        if (matrix[i][j]>0) matrix[j][i]=matrix[i][j];
 }
 
 void fill_matrix_4(vector<vector<int>> &matrix, vector<string> &labels, int &pic)
@@ -116,36 +117,9 @@ void fill_matrix_4(vector<vector<int>> &matrix, vector<string> &labels, int &pic
     matrix[i][j]=1;
     for (size_t i=0;i<matrix.size(); i++)
     for (size_t j=0;j<matrix.size(); j++)
-        if (matrix[i][j]==1) matrix[j][i]=1;
-}
-
-void fill_matrix_4_1(vector<vector<int>> &matrix, vector<string> &labels, int &pic)
-{
-    int n=10;
-    int a=0,b=1,c=2,d=3,e=4,f=5,g=6,h=7,i=8,j=9;
-    labels.push_back("ABCDEFGHIJ");
-    pic=3;
-    matrix=vector<vector<int>>(n,vector<int>(n,0));
-    matrix[a][b]=1;
-    matrix[a][c]=2;
-    matrix[a][f]=3;
-    matrix[b][e]=4;
-    matrix[b][j]=1;
-    matrix[c][d]=2;
-    matrix[d][f]=3;
-    matrix[d][e]=4;
-    matrix[d][g]=1;
-    matrix[e][i]=2;
-    matrix[e][j]=3;
-    matrix[f][g]=4;
-    matrix[f][h]=1;
-    matrix[g][h]=2;
-    matrix[h][i]=3;
-    matrix[i][j]=10;
-    for (size_t i=0;i<matrix.size(); i++)
-    for (size_t j=0;j<matrix.size(); j++)
         if (matrix[i][j]>0) matrix[j][i]=matrix[i][j];
 }
+
 
 void fill_matrix_5(vector<vector<int>> &matrix, vector<string> &labels, int &pic)
 {
@@ -172,7 +146,7 @@ void fill_matrix_5(vector<vector<int>> &matrix, vector<string> &labels, int &pic
     matrix[i][j]=1;
     for (size_t i=0;i<matrix.size(); i++)
     for (size_t j=0;j<matrix.size(); j++)
-        if (matrix[i][j]==1) matrix[j][i]=1;
+        if (matrix[i][j]>0) matrix[j][i]=matrix[i][j];
 }
 
 void reweight(vector<vector<int>> &matrix)
@@ -247,75 +221,81 @@ struct path
     int runner_up = 999999999;
     path(int l, vector<int> v):len(l),vertices(v){}
     path():len(-1),vertices(0){}
-    show(string labels)
+    void show(string labels) const
     {
-        cout << "\t"; for (int l(0); l<vertices.size(); l++) cout << labels[vertices[l]]; cout << endl << "\t" << len << endl;
+        for (int l(0); l<vertices.size(); l++) cout << labels[vertices[l]]; cout << endl << len << endl;
     }
     size_t size() {return vertices.size();}
+    bool const operator<(const path a) const {return vertices< a.vertices;}
 };
 
-void show_paths(vector<path> path_to, string labels)
+void show_paths(vector<set<path>> paths_to, string labels)
 {
-for (int k(0); k<path_to.size(); k++) {cout << labels[*(path_to[k].vertices.end()-1)] << ": " << path_to[k].len << "(+" << path_to[k].runner_up << "): ";for (int l(0); l<path_to[k].size(); l++) cout << labels[path_to[k].vertices[l]]; cout << endl;}
+    for (int p(0); p<paths_to.size(); p++)
+    {
+        cout << labels[*((*paths_to[p].begin()).vertices.end()-1)]<< ": " << endl;
+        auto it = paths_to[p].begin();
+        while (it!=paths_to[p].end())
+            {
+                (*it).show(labels);
+                it++;
+            }
+        cout << endl;
+    }
 }
 
-void all_paths(vector<path> &p, vector<vector<int>> matrix, string labels)
-{/*
+void all_paths(vector<path> &res, vector<vector<int>> matrix, string labels)
+{
     for (int i(0); i<matrix.size(); i++)
-        for (int j(i+1); j<matrix.size(); j++)*/int i = 2; int j = 5;
- //       {
+        for (int j(i+1); j<matrix.size(); j++)
+
+        {
             //clog << "finding path " << labels[i] << " -> " << labels[j] << endl;
-            vector<path> path_to(matrix.size());
-            for (int k(0); k<path_to.size(); k++) path_to[k] = path(999999, vector<int>(1, k));
-            path_to[i] = path(0, vector<int>(1, i));
+            vector<set<path>> paths_to(matrix.size());
+            paths_to[i].insert(path(0, vector<int>(1, i)));
             //show_paths(path_to, labels);
 
-            //while (*(path_here[0].vertices.end())!=j)
-//            {
-                //sort(path_to.begin(), path_to.end(), [](path a, path b){return a.len < b.len;});
-                //cout << endl;
-                //show_paths(path_to, labels);
-                for (int v(0); v<matrix.size(); v++)
-                {
-                    int cur_from = -1;//*(path_to[0].vertices.end()-1);
-                    for (int l(0); l<path_to.size(); l++)
-                        if (!path_to[l].done)
-                            if (cur_from == -1 or path_to[cur_from].len > path_to[l].len) cur_from = l;
-                    //cout << cur_from;
-                    for (int cur_to(0);cur_to<matrix.size();cur_to++)
-                    {
-                        if (matrix[cur_from][cur_to]>0)
-                        {
-                            clog<<"found " << labels[cur_from] << " - " << labels[cur_to] << endl;
-                            //for (int l(0); l<path_to[0].vertices.size(); l++) cout << labels[path_to[0].vertices[l]]; cout << endl;
-                            path cand_path = path(path_to[cur_from]);
-                            //for (int l(0); l<cand_path.vertices.size(); l++) cout << labels[cand_path.vertices[l]]; cout << endl;
-                            cand_path.vertices.push_back(cur_to);
-                            cand_path.len += matrix[cur_from][cur_to];
-                            int better = path_to[cur_to].len - cand_path.len;
-                            if (path_to[cur_to].len<999999 and better<0 and -better<path_to[cur_to].runner_up) {bool good(true); for (int m(0);m<cand_path.vertices.size(); m++) if (cand_path.vertices[m]==cur_to) {good=false; break;} if (good) path_to[cur_to].runner_up = -better;}
-                            if (path_to[cur_to].len==999999 or better>0)
-                                        {path_to[cur_to] = cand_path; path_to[cur_to].runner_up = better;}
-                        }
-                    }
-                    path_to[cur_from].done = true;
-
-                    cout << endl;
-                    show_paths(path_to, labels);
-                }
-  /*
-                active_queue.erase(active_queue.begin());
-                for (int cur_to(0); cur_to<matrix.size(); cur_to++)
+            for (int v(0); v<matrix.size(); v++)
+            {
+                for (int cur_from(0); cur_from<matrix.size(); cur_from++)
+                for (int cur_to(0);cur_to<matrix.size();cur_to++)
                 {
                     if (matrix[cur_from][cur_to]>0)
                     {
-
-                        active_queue.push_back(cur_to);
+                        //clog<<"found " << labels[cur_from] << " - " << labels[cur_to] << endl;
+                        auto it = paths_to[cur_from].begin();
+                        while (it!=paths_to[cur_from].end())
+                        {
+                            bool add = true;
+                            for (int q(0); q<(*it).vertices.size(); q++)
+                            if ((*it).vertices[q]==cur_to) {add=false; break;}
+                            if (add)
+                            {
+                                path cand_path = path(*it);
+                                cand_path.vertices.push_back(cur_to);
+                                cand_path.len += matrix[cur_from][cur_to];
+                                paths_to[cur_to].insert(cand_path);
+                            }
+                            it++;
+                        }
                     }
                 }
             }
-            for (int k(0); k<matrix.size(); k++) {cout << labels[k] << ": ";for (int l(0); l<path_here[k].size(); l++) cout << labels[path_here[k].vertices[l]]; cout << endl;}
-        }*/
+            for (int p(0); p<paths_to.size(); p++)
+            {
+                if (*((*(paths_to[p].begin())).vertices.end()-1) != j) continue;
+                vector<path> paths (paths_to[p].begin(), paths_to[p].end());
+                sort(paths.begin(), paths.end(), [](path a, path b){return a.len < b.len;});
+                if ((paths[1].len - paths[0].len == 1 or paths[1].len - paths[0].len == 2) and paths[0].vertices.size() > 5 and paths[0].vertices.size() > paths[1].vertices.size() and paths[1].vertices.size() > 4)
+                {
+                    //cout << labels[*((*paths_to[p].begin()).vertices.end()-1)] << ": ";
+                    //paths[0].show(labels);
+                    //paths[1].show(labels);
+                    //cout << endl;
+                    res.push_back(paths[0]);
+                }
+            }
+        }
 }
 
 void min_path()
@@ -328,7 +308,7 @@ void min_path()
     fill_matrix_4_1(matrix,labels,pic);
     vector<vector<int>> matrix_1(matrix.size(),vector<int>(matrix.size(),0));
     vector<int> order(matrix.size());
-    int tasks=1;
+    int tasks=30;
     for (int taski=0; taski<tasks;taski++)
     {
         ofs << "<question type=\"shortanswer\">" << endl
@@ -337,11 +317,18 @@ void min_path()
                 <<"</name>" << endl
                 <<"<questiontext format=\"html\">" << endl
                 <<"<text><![CDATA[<p>";
-        ofs << "ѕриведЄнна€ матрица смежности описывает граф, изображенный на рисунке. ѕоложительное число соответствует стоимости перехода по ребру из вершины в вершину, ноль - отсутствию ребра. ”кажите наименьшую стоимость пути из ";
+        ofs << "ѕриведЄнна€ матрица смежности описывает граф, изображенный на рисунке. ѕоложительное число соответствует стоимости перехода по ребру из вершины в вершину. ";
         vector<path> p;
-        all_paths(p, matrix, labels[0]);
+        while (p.size()==0)
+        {
+            reweight(matrix);
+            all_paths(p, matrix, labels[0]);
+        }
+        //p[0].show(labels[0]);
+        ofs << "”кажите наименьшую стоимость пути из " << labels[0][(*p[0].vertices.begin())] << " в " << labels[0][(*(p[0].vertices.end()-1))] << "." << endl;
         matrix_1.clear();
         for (size_t i=0; i<order.size(); i++) order[i]=i;
+        random_shuffle(order.begin(),order.end());
         ofs << "  ";
         ofs << "<table border=\"1\"><tr><td></td>" <<endl;
         for (size_t i=1; i<=order.size(); i++)
@@ -350,7 +337,7 @@ void min_path()
         for (size_t i=0; i<order.size(); i++)
         {
             ofs << "<tr><th>V"<<i+1 << "</th>";
-            for (size_t j=0; j<order.size(); j++) ofs << "<td>"<<matrix[order[i]][order[j]] << "</td>";
+            for (size_t j=0; j<order.size(); j++) ofs << "<td>"<<(matrix[order[i]][order[j]]==0?"":to_string(matrix[order[i]][order[j]])) << "</td>";
             ofs << "</tr>"<<endl;
         }
         ofs << "</table></p>" <<endl;
@@ -363,7 +350,7 @@ void min_path()
         for (int i=0;i<labels.size(); i++)
         {
             ofs << "<answer fraction=\"100\" format=\"moodle_auto_format\"><text>";
-            for (auto j:order) ofs << labels[i][j];
+            ofs << p[0].len;
             ofs << "</text><feedback format=\"html\"><text></text></feedback></answer>"<<endl;
         }
         ofs << "</question>"<<endl<<endl;
