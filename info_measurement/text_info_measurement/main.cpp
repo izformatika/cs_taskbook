52c7e03ec9a50138cc3a39e43eaefb3ea1f42d1a
izformatika <izformatika@gmail.com>
2021-05-12 14:01:11 +0300

    code for g7_3_2_1_1_1 ready, answer wrong

--- info_measurement/text_info_measurement/main.cpp
+++ info_measurement/text_info_measurement/main.cpp
@@ -1,4 +1,5 @@
 #include <ctime>
+#include <map>
 #include <random>
 #include <iostream>
 #include <sstream>
@@ -65,6 +66,100 @@ void g7_1_1_2_2_1(int task_qtty, ofstream &ofs, int solution_time=0)
     ofs << "</quiz>" << endl;
     #endif
 }
+
+void g7_3_2_1_1(int task_qtty, ofstream &ofs, int solution_time=0)
+{
+    mt19937 mt(time(0));
+    set<int> ans;
+
+    int done(0);
+    array<int,3> small_primes={3,5,7};
+    uniform_int_distribution<>uid1_10(1, 10);
+    uniform_int_distribution<>uid0_2(0,2);
+    uniform_int_distribution<>uid0_1(0,1);
+    uniform_int_distribution<>uid1_16(1,16);
+    uniform_int_distribution<>uid1_200(1,200);
+    uniform_int_distribution<>uid_primes(0, small_primes.size()-1);
+    while(done<task_qtty)
+    {
+        int t1(uid1_10(mt)*10);
+        int ch1(pow(2, uid0_2(mt)));
+        int freq1(uid1_16(mt)*2);
+        int sz1(uid1_200(mt));
+        double i1=double(sz1)/(t1*ch1*freq1*1000);
+        if (((int(i1*1000))%10)==((int((i1*1000))%100)/10)) continue;//bits per symbol - too simple for a number
+
+        int td(pow(2,uid0_2(mt)+1));
+        int ch2;
+        do{ ch2=pow(2, uid0_2(mt));}while(ch2==ch1);
+        int freqd(pow(2,uid0_2(mt)+1));
+        if(uid0_1(mt)==0) {td*=-1;freqd*=small_primes[uid_primes(mt)];}
+        else {freqd*=-1;td*=small_primes[uid_primes(mt)];}
+
+        int sz2=sz1;
+        if (td<0) sz2/=-td;
+        else sz2*=td;
+        if (freqd<0) sz2/=-freqd;
+        else sz2*=freqd;
+        sz2=sz2/ch1*ch2;
+
+        int chk=sz2;
+        if (td<0) chk*=-td;
+        else chk/=td;
+        if (freqd<0) chk*=-freqd;
+        else chk/=freqd;
+        chk=chk/ch2*ch1;
+        if (chk!=sz1) continue;//divisions spoiled the answer
+
+        if (ans.find(int(sz2))!=ans.end()) continue;
+
+        ostringstream task_txt;
+        ostringstream answer_txt;
+        map<int, string> channel_desc;
+        channel_desc[1]= "моно (1 канал)";
+        channel_desc[2]= "стерео (2 канала)";
+        channel_desc[4]= "квадро (4 канала)";
+        task_txt << "Звукозапись длительностью " << t1 << " секунд сохранили в формате " << channel_desc[ch1] << " с частотой дискретизации "
+            << freq1 << " килогерц" << (freq1%10>=2 and freq1%10<=4 and (freq1%100<11 or freq1%100>15)?"а":"")
+            << ". Размер файла оказался равным " << sz1 << " кибибайт" << (sz1%10==1 and sz1%100!=11?"у":"ам")
+            << ". Сжатие данных не производилось, другой информации кроме звука в файле не было. Пользователь ";
+        int t2;
+        if (td<0) {task_txt << "скопировал фрагмент звукозаписи длительностью " ; t2=t1/(-td);}
+        else {task_txt << "добавил к записи фрагмент длительностью "; t2=t1*td-t1; if (t2>t1/2) continue;}
+
+        done++;
+        ans.insert(sz2);
+
+        int freq2((freqd<0?freq1/(-freqd):freq1*freqd));
+        task_txt << t2 << " секунд" << (t2%10>=2 and t2%10<=4 and (t2%100<11 or t2%100>15)?"ы":"")
+            << ", перекодировал его в формат " << channel_desc[ch2] << ", "
+            << (freqd<0?"уменьшил":"увеличил") << " частоту дискретизации до " << freq2
+            << " килогерц" << (freq2%10>=2 and freq2%10<=4 and (freq2%100<11 or freq2%100>15)?"а":"")
+            << " и сохранил полученный результат в новом файле без сжатия данных и без дополнительных данных кроме звука. <br/>"
+            << "Определите объём полученного файла в кибибайтах. В ответе укажите только число, округлённое до целых в большую сторону, без единиц измерения.<br/><br/>";
+                task_txt <<"Примечание. 1 мебибайт - 1024 кибибайта, 1 кибибайт - 1024 байта. Это технически более корректные термины, чем 'килобайт' и 'мегабайт'. Приняты в 1999 году Международной электротехнической комиссией в стандарте IEC 60027-2, в России в ГОСТ 8.417-2002.";
+            answer_txt << sz2;
+
+        #if moodle
+
+        moodle_write_question(ofs,done,task_txt.str(), answer_txt.str());
+        #else
+        cout << task_txt.str() << endl <<answer_txt.str() <<endl;
+        #endif
+        if (done==task_qtty)
+        {
+            #if moodle
+            ofs << "</quiz>" << endl;
+            #endif
+                return;
+        }
+    }
+    #if moodle
+    ofs << "</quiz>" << endl;
+    #endif
+}
+
+
 int factorial(int a)
 {
     int ans(1);
@@ -118,7 +213,8 @@ int main()
     #endif
     setlocale(LC_ALL,"Russian");
 
-    g10_1_1(50, ofs);
+    //g10_1_1(50, ofs);
+    g7_3_2_1_1(10, ofs);
 
     return 0;
 }
