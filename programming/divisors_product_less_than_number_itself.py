2cd48fb995671f618a0b7d82874d047a70fb4eb5
ioann <ioann.nikulin@gmail.com>
2021-11-17 20:40:54 +0300

    div product less than original: another strategy, collect at least 5 numbers with same M(N) endings and make them an answer

--- programming/divisors_product_less_than_number_itself.py
+++ programming/divisors_product_less_than_number_itself.py
@@ -27,78 +27,88 @@ def shortanswer(name, question, answer, file):
 
 from math import ceil, floor, sqrt, log
 from random import randint
-
-rainbow_table = {}
+import winsound
+#rainbow_table = {}
 
 def divisors(x, first):
     div = []
-    lim = x**(1//4)
+    divprod = 1
+    lim = x**(1/first)
     for i in range(2, x//2+1):
         if x%i==0:
             div.append(i)
+            divprod *= i
+            if divprod>x:
+                return []
             if len(div) >= first:
-                return div
-        if i>lim and len(div):
+                return div            
+            lim = (x//divprod)**(1/(first - len(div)))
+        if i>lim:
             return []
     return div
 
-for f in range(200000000, 300000000):
-    if f%1000 == 0:
-        print(f)
-    d = divisors(f, 8)
-    if len(d)==8:
-        rainbow_table[f] = d
+ending_len = 2
+modby = 10**ending_len
 
-def gen_task(f, div_qtty, num_qtty, ending):
-    modby = 10**ceil(log(ending, 10))
-    ans = []
-    
+def gen_task(f, div_qtty, num_qtty):
+    endings = {}
     for i in range(f+1, f+1000001):
-        divs = rainbow_table[i]
+        divs = divisors(i, div_qtty)
         if len(divs)<div_qtty:
             continue
         divprod = 1
         for j in divs[:div_qtty]:
             divprod*=j
-        if divprod<i and divprod%modby == ending:
-            ans.append(divprod)
-            if len(ans)>=num_qtty:
-                return ans
+        if divprod<i:
+            ending = divprod%modby
+            if ending not in endings.keys():
+                endings[ending] = [divs[:div_qtty]]
+            else:
+                endings[ending].append(divs[:div_qtty])
+                if len(endings[ending]) == num_qtty:
+                    if i-f<100:
+                        return []
+                    ans = []
+                    for i in endings[ending]:
+                        cur = 1
+                        for j in i:
+                            cur*=j
+                        ans.append(cur)
+                    return ans
     return []
      
 answers = []
-file = open("data.xml", 'w', encoding='utf-8')
+file = open("data.xml", 'a', encoding='utf-8')
 for f in range(200000000, 500000000, 1000000):
     for div_qtty in range(5, 8):
         for num_qtty in range(5, 8):
-            for ending in range(10, 99):
-                if ending % 10 == 0:
-                    print(ending)
-                ans_i=gen_task(f, div_qtty, num_qtty, ending)
-                task_txt = f"Пусть M (N) – произведение {div_qtty} наименьших различных натуральных делителей натурального числа N, не считая единицы. Если у числа N меньше {div_qtty} таких делителей, то M (N) считается равным нулю.<br/>\nНайдите {num_qtty} наименьших натуральных чисел, превышающих {f}, для которых 0 < M (N) < N, и M(N) заканчивается на {ending}. <br/>\nВ ответе запишите через пробел только найденные значения M (N) в порядке возрастания соответствующих им чисел N (но не обязательно в порядке возрастания M (N)).<br/>\n<i>Источник задания: диагностическая работа Статград по информатике 27.11.2021, сборник К.Ю. Полякова №25.183.</i>"
-                
-                if ans_i is None:
-                    continue
-                if len(ans_i)<num_qtty or ans_i in answers:
-                    print(':(')
-                    continue
-                bad = True
-                for i in range(len(ans_i)-1):
-                    if ans_i[i]>ans_i[i+1]:
-                        bad = False
-                        break
-                if bad: continue
-                answers.append(ans_i)
-                ans = ' '.join(list(map(str, ans_i)))
-                shortanswer(f'{len(answers):0>2}', task_txt, ans, file)
-                if len(ans) >= 30:
-                    break   
-            if len(ans) >= 30:
-                break         
-        if len(ans) >= 30:
+            print(num_qtty)
+            ans_i=gen_task(f, div_qtty, num_qtty)
+            if ans_i is None or len(ans_i)<num_qtty:
+                continue
+            ending = ans_i[0]%modby
+            if ending in answers:
+                continue            
+            
+            task_txt = f"Пусть M (N) – произведение {div_qtty} наименьших различных натуральных делителей натурального числа N, не считая единицы. Если у числа N меньше {div_qtty} таких делителей, то M (N) считается равным нулю.<br/>\nНайдите {num_qtty} наименьших натуральных чисел, превышающих {f}, для которых 0 < M (N) < N, и M(N) заканчивается на {ending:0>{ending_len}}. <br/>\nВ ответе запишите через пробел только найденные значения M (N) в порядке возрастания соответствующих им чисел N (но не обязательно в порядке возрастания M (N)).<br/>\n<i>Источник задания: диагностическая работа Статград по информатике 27.11.2021, сборник К.Ю. Полякова №25.183.</i>"
+            
+            bad = True
+            for i in range(len(ans_i)-1):
+                if ans_i[i]>ans_i[i+1]:
+                    bad = False
+                    break
+            if bad: continue
+            answers.append(ending)
+            ans = ' '.join(list(map(str, ans_i)))
+            shortanswer(f'{len(answers):0>2}', task_txt, ans, file)
+            file.flush()
+            winsound.Beep(2500, 1000)
+            if len(answers) >= 30:
+                break          
+        if len(answers) >= 30:
             break               
-    if len(ans) >= 30:
+    if len(answers) >= 30:
         break
 file.close()
-import winsound
+
 winsound.Beep(2500, 1000)
