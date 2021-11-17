b5bbb01468c855380d8633a36e44ae92f72af8f8
ioann <ioann.nikulin@gmail.com>
2021-11-17 20:00:37 +0300

    product of min divisors less than original: tried to generate with all possible endings of product, but failed

--- programming/divisors_product_less_than_number_itself.py
+++ programming/divisors_product_less_than_number_itself.py
@@ -25,50 +25,80 @@ def shortanswer(name, question, answer, file):
     file.write("</answer>\n")
     file.write("</question>\n\n" )
 
-from math import ceil, floor, sqrt
-def gen_task(f, div_qtty, num_qtty):
-    ans = []
-    def divisors(x):
-        div = [1, x]
-        for i in range(2, floor(sqrt(x))):
-            if x%i==0:
-                div.append(i)
-                div.append(x//i)
-        if sqrt(x) == floor(sqrt(x)):
-            div.append(sqrt(x))
-        div.sort()
-        return div
+from math import ceil, floor, sqrt, log
+from random import randint
 
-    for i in range(f+1, f+10001):
-        divs = divisors(i)    
+rainbow_table = {}
+
+def divisors(x, first):
+    div = []
+    lim = x**(1//4)
+    for i in range(2, x//2+1):
+        if x%i==0:
+            div.append(i)
+            if len(div) >= first:
+                return div
+        if i>lim and len(div):
+            return []
+    return div
+
+for f in range(200000000, 300000000):
+    if f%1000 == 0:
+        print(f)
+    d = divisors(f, 8)
+    if len(d)==8:
+        rainbow_table[f] = d
+
+def gen_task(f, div_qtty, num_qtty, ending):
+    modby = 10**ceil(log(ending, 10))
+    ans = []
+    
+    for i in range(f+1, f+1000001):
+        divs = rainbow_table[i]
         if len(divs)<div_qtty:
             continue
         divprod = 1
-        for j in divs[:div_qtty+1]:
+        for j in divs[:div_qtty]:
             divprod*=j
-        if divprod<i:
+        if divprod<i and divprod%modby == ending:
             ans.append(divprod)
             if len(ans)>=num_qtty:
                 return ans
-            
+    return []
+     
 answers = []
 file = open("data.xml", 'w', encoding='utf-8')
-for f in range(200000000, 1000000000, 1000000):
+for f in range(200000000, 500000000, 1000000):
     for div_qtty in range(5, 8):
         for num_qtty in range(5, 8):
-            task_txt = f"Пусть M (N) – произведение {div_qtty} наименьших различных натуральных делителей натурального числа N, не считая единицы. Если у числа N меньше {div_qtty} таких делителей, то M (N) считается равным нулю.<br/>\nНайдите {num_qtty} наименьших натуральных чисел, превышающих {f}, для которых 0 < M (N) < N. <br/>\nВ ответе запишите через пробел только найденные значения M (N) в порядке возрастания соответствующих им чисел N (но не обязательно в порядке возрастания M (N)).<br/>\n<i>Источник задания: диагностическая работа Статград по информатике 27.11.2021.</i>"
-            ans_i=gen_task(f, div_qtty, num_qtty)
-            if len(ans_i)<num_qtty or ans_i in answers:
-                continue
-            bad = True
-            for i in range(len(ans_i)-1):
-                if ans_i[i]>ans_i[i+1]:
-                    bad = False
-                    break
-            if bad: continue
-            answers.append(ans_i)
-            ans = ' '.join(list(map(str, ans_i)))
-            shortanswer(f'{len(answers):0>2}', task_txt, ans, file)
+            for ending in range(10, 99):
+                if ending % 10 == 0:
+                    print(ending)
+                ans_i=gen_task(f, div_qtty, num_qtty, ending)
+                task_txt = f"Пусть M (N) – произведение {div_qtty} наименьших различных натуральных делителей натурального числа N, не считая единицы. Если у числа N меньше {div_qtty} таких делителей, то M (N) считается равным нулю.<br/>\nНайдите {num_qtty} наименьших натуральных чисел, превышающих {f}, для которых 0 < M (N) < N, и M(N) заканчивается на {ending}. <br/>\nВ ответе запишите через пробел только найденные значения M (N) в порядке возрастания соответствующих им чисел N (но не обязательно в порядке возрастания M (N)).<br/>\n<i>Источник задания: диагностическая работа Статград по информатике 27.11.2021, сборник К.Ю. Полякова №25.183.</i>"
+                
+                if ans_i is None:
+                    continue
+                if len(ans_i)<num_qtty or ans_i in answers:
+                    print(':(')
+                    continue
+                bad = True
+                for i in range(len(ans_i)-1):
+                    if ans_i[i]>ans_i[i+1]:
+                        bad = False
+                        break
+                if bad: continue
+                answers.append(ans_i)
+                ans = ' '.join(list(map(str, ans_i)))
+                shortanswer(f'{len(answers):0>2}', task_txt, ans, file)
+                if len(ans) >= 30:
+                    break   
+            if len(ans) >= 30:
+                break         
+        if len(ans) >= 30:
+            break               
+    if len(ans) >= 30:
+        break
 file.close()
 import winsound
 winsound.Beep(2500, 1000)
