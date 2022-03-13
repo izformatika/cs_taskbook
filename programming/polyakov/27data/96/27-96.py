048d28b1b36608d3c2086d2861af59bf4707d3d6
izformatika <izformatika@gmail.com>
2022-03-13 23:36:33 +0300

    fixed 96 to check div by different numbers

--- programming/polyakov/27data/96/27-96.py
+++ programming/polyakov/27data/96/27-96.py
@@ -1,36 +1,49 @@
 #coding:utf-8
-f = open("1.txt", "w")
-from random import randint
-n = 20
-f.write(f"{n}\n")
-for i in range(n):
-  f.write(f"{randint(1, 10000)}\n")
-f.close()
-
-f = open("2.txt", "w")
+gen = True
+example = True
+two = True
 from random import randint, shuffle
-n = 60000
-f.write(f"{n}\n")
-for i in range(n):
-  f.write(f"{randint(1, 10000)}\n")
-f.close()
 
-numbers = [2,3,5,7,11,13,17,19,23,29,31,37]
-shuffle(numbers)
-five = numbers.pop()
-seven = numbers.pop()
-numbers1 = numbers[::1]
-numbers1[1]=five
-numbers1[2]*=five
-numbers1[5]*=five
-numbers1[4]*=seven
-numbers1[7]*=seven
-numbers1[8]=five
-print()
-print(five, seven)
-print(10, *numbers1, sep = '\n')
-print(f'В этом наборе максимальную сумму элементов {sum(numbers1[2:8])} имеет подпоследовательность ({", ".join(map(str, numbers1[2:8]))}), содержащая два числа делящихся на {five} ({numbers1[2]} и {numbers1[5]}) и два числа, делящихся на {seven} ({numbers1[4]} и {numbers1[7]}).')
-print()
+if gen:
+  f = open("1.txt", "w")
+  from random import randint
+  n = 50
+  f.write(f"{n}\n")
+  for i in range(n):
+    f.write(f"{randint(1, 10000)}\n")
+  f.close()
+  
+  if two:
+    f = open("2.txt", "w")
+    from random import randint, shuffle
+    n = 60000
+    f.write(f"{n}\n")
+    for i in range(n):
+      f.write(f"{randint(1, 10000)}\n")
+    f.close()
+    
+if not example:
+  numbers = [2,3,5,7,11,13,17,19,23,29,31,37]
+  shuffle(numbers)
+  five = numbers.pop()
+  seven = numbers.pop()
+else:
+  numbers = [2,3,5,7,11,13,17,19,23,29,31,37]
+  shuffle(numbers)
+  five = numbers.pop()
+  seven = numbers.pop()
+  numbers1 = numbers[::1]
+  numbers1[1]=five
+  numbers1[2]*=five
+  numbers1[5]*=five
+  numbers1[4]*=seven
+  numbers1[7]*=seven
+  numbers1[8]=five
+  print()
+  print(five, seven)
+  print(10, *numbers1, sep = '\n')
+  print(f'В этом наборе максимальную сумму элементов {sum(numbers1[2:8])} имеет подпоследовательность ({", ".join(map(str, numbers1[2:8]))}), содержащая два числа делящихся на {five} ({numbers1[2]} и {numbers1[5]}) и два числа, делящихся на {seven} ({numbers1[4]} и {numbers1[7]}).')
+
 
 for iii in (1,2):
 
@@ -55,6 +68,6 @@ for iii in (1,2):
       if totalSum < tailMinSum[count5-count7]:
         tailMinSum[count5-count7] = totalSum
   
-  print( maxSum )
-  if maxSum == 0 or maxSum == totalSum:
+  print( maxSum , totalSum, count5, count7)
+  if (maxSum < totalSum*0.3 and n < 1000) or maxSum > totalSum*0.67 or count5 < 4 or count7 < 4:
     print("generate again")  
